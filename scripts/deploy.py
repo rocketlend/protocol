87b897dabfac9069903a037a63ed0142aecc7f93
Ramana Kumar <ramana@member.fsf.org>
2024-06-06 17:23:02 +0100

    Grab RPL for deployer on forked testnet deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,7 @@
 import IPython
 import warnings
-from ape import project, networks, accounts
+from eth_utils import keccak
+from ape import project, networks, accounts, Contract
 
 rocketStorageAddresses = dict(
         mainnet='0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46',
@@ -11,7 +12,15 @@ def main():
     provider_uri = networks.active_provider.uri
     if network_name != 'holesky-fork':
         raise f'Only holesky-fork is currently supported, not {network_name}'
-    accounts.test_accounts[0].deploy(project.rocketlend, rocketStorageAddresses['holesky'])
+    deployer = accounts.test_accounts[0]
+    deployer.deploy(project.rocketlend, rocketStorageAddresses['holesky'])
     print(f'RPC available at {provider_uri}')
+    rocketStorage = Contract(rocketStorageAddresses['holesky'])
+    rocketVault = accounts[rocketStorage.getAddress(keccak('contract.addressrocketVault'.encode()))]
+    RPLToken = Contract(rocketStorage.getAddress(keccak('contract.addressrocketTokenRPL'.encode())))
+    amountRPL = 1000
+    amount = amountRPL * 10 ** RPLToken.decimals()
+    RPLToken.transfer(deployer, amount, sender=rocketVault)
+    print(f'Grabbed {amountRPL} RPL for {deployer.address} with private key {deployer.private_key}')
     warnings.filterwarnings('ignore')
     IPython.embed()
